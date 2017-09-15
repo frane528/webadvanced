64b848563130b9e5197eb27c824bbbe9931ea366
Emily Frank <frane528@newschool.edu>
2017-09-15 14:39:38 +0000

    new edits

--- console-test/script.js
+++ console-test/script.js
@@ -1,24 +1,17 @@
 function shapes(){
-      var redSquare = document.getElementById("redSquare");
-      var yellowCircle = document.getElementById("yellowCircle");
-      var blueSquare = document.getElementById("blueSquare");
+      var shapeGroup = document.getElementById("shapeGroup");
       var breakLine = '\n';
       
       var result = '';
       
       var width = document.getElementById('rowNumber').value;
       var height = document.getElementById('colNumber').value;
-      
+    
       for(var j=0;j<height;j++){
         for(var i=0;i<width;i++){
-          result += redSquare;
-        }; result += breakLine;
-        for(var i=0;i<width;i++){
-          result += yellowCircle;
-        }; result += breakLine;
-        for(var i=0;i<width;i++){
-          result += blueSquare;
-        }; result += breakLine;
+          result += shapeGroup;
+        };
+        
       };
       console.log(result);
     };
