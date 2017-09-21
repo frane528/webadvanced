da346ec02bc64eef62c69ef6c7c41c8111a8c351
Emily Frank <frane528@newschool.edu>
2017-09-21 20:01:09 +0000

    new js file

---
+++ console-test/classwork-script.js
@@ -0,0 +1,28 @@
+var shapeTimer = 0; //declare our timer for setTimeout
+
+
+for (var i = 0; i < 300; i++) { //standard for loop to iterate for 100 console lines
+    setTimeout(function() { // introduce the setTimout function
+        var shape = ""; //empty string that will be filled below!
+
+        for (var s = 0; s < 30; s++) { //for loop stating 30 characters per line
+
+            var randomShape = Math.random()*100; 
+            // use randomShape to determine what combination of shapes and " " each line has
+            if (randomShape < 20) {
+                shape += "🔷"; //add blue diamond to string
+            }else if (randomShape > 20 && randomShape < 40){
+                shape += "🔴"; //add red circle to string
+            }else if (randomShape > 40 && randomShape < 60){
+                shape += "🔶"; //add yellow diamond to string
+            }else if (randomShape > 60 && randomShape < 80){
+                shape += "🔵"; //add yellow diamond to string
+            }else {
+                shape += " "; //add space to string
+            }
+
+        }
+        console.log(shape); //output of snow string
+    }, shapeTimer); // snowTimer determines when the sleet string is released
+    shapeTimer += 300; // in order to "animate", each sleet string is released +100 milliseconds after the previous one.
+} 
--- console-test/script.js
+++ console-test/script.js
@@ -1,28 +1,14 @@
-var shapeTimer = 0; //declare our timer for setTimeout
+var shapeTimer = 0;
+let shapeArray = ['🔴🔵⚫️🔴🔵⚫️🔴🔵⚫️🔴🔵⚫️',' 🔵⚫️🔴🔵⚫️🔴🔵⚫️🔴🔵','   ⚫️🔴🔵⚫️🔴🔵⚫️','      🔴🔵⚫️','        🔵','        ⚫️','        🔴','      🔵⚫️🔴','   🔴🔵⚫️🔴🔵⚫️🔴'];
 
-
-for (var i = 0; i < 300; i++) { //standard for loop to iterate for 100 console lines
-    setTimeout(function() { // introduce the setTimout function
-        var shape = ""; //empty string that will be filled below!
-
-        for (var s = 0; s < 30; s++) { //for loop stating 30 characters per line
-
-            var randomShape = Math.random()*100; 
-            // use randomShape to determine what combination of shapes and " " each line has
-            if (randomShape < 20) {
-                shape += "🔷"; //add blue diamond to string
-            }else if (randomShape > 20 && randomShape < 40){
-                shape += "🔴"; //add red circle to string
-            }else if (randomShape > 40 && randomShape < 60){
-                shape += "🔶"; //add yellow diamond to string
-            }else if (randomShape > 60 && randomShape < 80){
-                shape += "🔵"; //add yellow diamond to string
-            }else {
-                shape += " "; //add space to string
-            }
-
-        }
-        console.log(shape); //output of snow string
-    }, shapeTimer); // snowTimer determines when the sleet string is released
-    shapeTimer += 300; // in order to "animate", each sleet string is released +100 milliseconds after the previous one.
-} 
+setTimeout(function() {
+  for (i = 0; i <5; i++) {
+    for (let j = 0; j < shapeArray.length; j++) {
+      for (let j = 1; j < shapeArray.length; j++) {
+      console.log(shapeArray[j]);
+    }
+  }
+}
+}, shapeTimer); 
+    shapeTimer += 300; 
+      
