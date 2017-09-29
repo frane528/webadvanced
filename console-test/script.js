9b39c562ed77db38f2e832c19acb72b670989b2a
Emily Frank <frane528@newschool.edu>
2017-09-29 11:44:04 +0000

    js file

--- console-test/script.js
+++ console-test/script.js
@@ -1,14 +1,18 @@
 function triangleAnimation(){
-  var result = '';
-  var t = 0;
+  var shape ="";
+  let triangleArrayOne = ['    ⚠️', '\n', '  ⚠️⚠️⚠️', '\n', ' ⚠️⚠️⚠️⚠️⚠️', '\n', '⚠️⚠️⚠️⚠️⚠️⚠️⚠️'];
+  let triangleArrayTwo = ['  ◢ ◣', '\n','◢◢ ◣◣'];
   
-  for(var i=0;i<1;i++){
-    setTimeout(function(){
-      result += '🔺';
-      console.log(result);
-    },t)
-    t+=100;
+  
+  var smallOrBig = Math.random ()*3;
+  if (smallOrBig < 1) {
+   shape += "🔺";
+  } else if (smallOrBig > 1 && smallOrBig < 2) {
+    shape += triangleArrayOne;
+  } else {
+    shape += triangleArrayTwo;
   }
+  console.log(shape);
 }
 
 function squareAnimation(){
