a6a40d5abca9dff43199c50dc61728044b6814a9
Emily Frank <frane528@newschool.edu>
2017-10-06 11:02:58 +0000

    js

--- console-new/js/index.js
+++ console-new/js/index.js
@@ -3,24 +3,21 @@ $('h1').click(function() {
 });
 
 $(window).resize(function(){
+
     if($(window).width() < 1499 ) {
         $('#target, h1, .shape').hide();}
-     else {
-     $('#target, h1, .shape').show();
+     else {$('#target, h1, .shape').show();
   }
-});
 
-$(window).resize(function(){
     if($(window).width() < 1000 ) {
         $('#almost').show();}
     else {$("#almost").hide();
     }
-});
 
-$(window).resize(function(){
+    $(window).resize(function(){
     if($(window).width() < 700 ) {
         $('#perfect').show();}
-  else {
-     $("#perfect").hide();
+    else {$("#perfect").hide();
   }
+  
 });
