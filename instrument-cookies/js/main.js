cc0fcbf525a2c15fd9fb09499b3e59034a5395f5
Emily Frank <frane528@newschool.edu>
2017-11-03 13:51:20 +0000

    files

--- instrument-cookies/js/main.js
+++ instrument-cookies/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     
-
+/*
 $(".instrument").click(function(){
     
     $(".instrument").each(function(){
@@ -15,39 +15,39 @@ $(".instrument").click(function(){
         
         if($(this).hasClass("big-instrument")){
            
-           $(this).animate({top: '8em', left: '5em', height: '100%'}) 
+           $(this).css({top: '8em', left: '5em', height: '100%'}) 
             
         }else{
             
             if($(this).attr("id") == "bass-button"){
                 //specific small position
-                $("#drum-button").animate({top: '28em', left: '10em', height: '10em'}, 500);
-                $("#sax-button").animate({top: '28em', height: '10em'}, 500);
-                $("#trumpet-button").animate({top: '28em', height: '10em'}, 500);
+                $("#drum-button").css({top: '28em', left: '10em', height: '10em'}, 500);
+                $("#sax-button").css({top: '28em', height: '10em'}, 500);
+                $("#trumpet-button").css({top: '28em', height: '10em'}, 500);
                 
             }
             
             if($(this).attr("id") == "drum-button"){
                 //specific small position
-                $("#bass-button").animate({top: '28em', left: '10em', height: '10em'}, 500);
-                $("#sax-button").animate({top: '28em', height: '10em'}, 500);
-                $("#trumpet-button").animate({top: '28em', height: '10em'}, 500);
+                $("#bass-button").css({top: '28em', left: '10em', height: '10em'}, 500);
+                $("#sax-button").css({top: '28em', height: '10em'}, 500);
+                $("#trumpet-button").css({top: '28em', height: '10em'}, 500);
                 
             }
             
             if($(this).attr("id") == "sax-button"){
                 //specific small position
-                $("#bass-button").animate({top: '28em', left: '10em', height: '10em'}, 500);
-                $("#drum-button").animate({top: '28em', height: '10em'}, 500);
-                $("#trumpet-button").animate({top: '28em', height: '10em'}, 500);
+                $("#bass-button").css({top: '28em', left: '10em', height: '10em'}, 500);
+                $("#drum-button").css({top: '28em', height: '10em'}, 500);
+                $("#trumpet-button").css({top: '28em', height: '10em'}, 500);
                 
             }
             
             if($(this).attr("id") == "trumpet-button"){
                 //specific small position
-                $("#bass-button").animate({top: '28em', left: '10em', height: '10em'}, 500);
-                $("#drum-button").animate({top: '28em', height: '10em'}, 500);
-                $("#sax-button").animate({top: '28em', height: '10em'}, 500);
+                $("#bass-button").css({top: '28em', left: '10em', height: '10em'}, 500);
+                $("#drum-button").css({top: '28em', height: '10em'}, 500);
+                $("#sax-button").css({top: '28em', height: '10em'}, 500);
                 
             }
             
@@ -73,9 +73,9 @@ $(".instrument").click(function(){
         bassAudio.get(0).play();
         });
 })
-
+*/
     
-/*
+
     var drumAudio = $("#drum-clip");
     var drumClicked = false;
     
@@ -85,17 +85,15 @@ $(".instrument").click(function(){
         
         
         if(!drumClicked){
-            $("#bass-button").animate({top: '28em', left: '30em', height: '10em'}, 500);
-            $("#sax-button").animate({top: '28em', height: '10em'}, 500);
-            $("#trumpet-button").animate({top: '28em', height: '10em'}, 500);
-            $("#guitar-button").animate({top: '28em', left: '50em', height: '10em'}, 500);      
+            $("#bass-button").css({top: '17em', height: '10em'}, 500);
+            $("#sax-button").css({top: '17em', height: '10em'}, 500);
+            $("#trumpet-button").css({top: '18em', height: '10em'}, 500);
             
             drumClicked = true;
         } else{
-            $("#bass-button").animate({top: '-4em', left: '40em', height: '100%'}, 500);
-            $("#sax-button").animate({top: '11em', height: '100%'}, 500);
-            $("#trumpet-button").animate({top: '10em', left: '20em', height: '100%'}, 500);
-            $("#guitar-button").animate({top: '-4em', left: '60em', height: '100%'}, 500);
+            $("#bass-button").css({top: '4em', height: '100%'}, 500);
+            $("#sax-button").css({top: '4em', height: '100%'}, 500);
+            $("#trumpet-button").css({top: '4em', height: '100%'}, 500);
         
             drumClicked = false;
         }
@@ -110,17 +108,15 @@ $(".instrument").click(function(){
         bassAudio.get(0).play();
         
         if(!bassClicked){
-            $("#drum-button").animate({top: '28em', left: '30em', height: '10em'}, 500);
-            $("#sax-button").animate({top: '28em', height: '10em'}, 500);
-            $("#trumpet-button").animate({top: '28em', height: '10em'}, 500);
-            $("#guitar-button").animate({top: '28em', left: '50em', height: '10em'}, 500);      
+            $("#drum-button").css({top: '17em', height: '10em'}, 500);
+            $("#sax-button").css({top: '17em', height: '10em'}, 500);
+            $("#trumpet-button").css({top: '17em', height: '10em'}, 500);
             
             bassClicked = true;
         } else{
-            $("#drum-button").animate({top: '-4em', left: '20em', height: '100%'}, 500);
-            $("#sax-button").animate({top: '11em', height: '100%'}, 500);
-            $("#trumpet-button").animate({top: '10em', left: '20em', height: '100%'}, 500);
-            $("#guitar-button").animate({top: '-4em', left: '60em', height: '100%'}, 500);
+            $("#drum-button").css({top: '4em', height: '100%'}, 500);
+            $("#sax-button").css({top: '4em', height: '100%'}, 500);
+            $("#trumpet-button").css({top: '4em', height: '100%'}, 500);
         
             bassClicked = false;
         }
@@ -136,17 +132,15 @@ $(".instrument").click(function(){
         saxAudio.get(0).play();
         
         if(!saxClicked){
-            $("#sax-button").animate({top: '-3em', left: '3em', height: '100%'}, 500);
-            $("#drum-button").animate({top: '28em', left: '10em', height: '10em'}, 500);
-            $("#trumpet-button").animate({top: '28em', height: '10em'}, 500);
-            $("#bass-button").animate({top: '28em', left: '30em', height: '10em'}, 500);      
+            $("#drum-button").css({top: '17em', height: '10em'}, 500);
+            $("#trumpet-button").css({top: '18em', height: '10em'}, 500);
+            $("#bass-button").css({top: '17em', height: '10em'}, 500);      
             
             saxClicked = true;
         } else{
-            $("#sax-button").animate({top: '11em', height: '100%'}, 500);
-            $("#drum-button").animate({top: '-4em', left: '20em', height: '100%'}, 500);
-            $("#trumpet-button").animate({top: '10em', left: '20em', height: '100%'}, 500);
-            $("#bass-button").animate({top: '-4em', left: '40em', height: '100%'}, 500);
+            $("#drum-button").css({top: '4em', height: '100%'}, 500);
+            $("#trumpet-button").css({top: '4em', height: '100%'}, 500);
+            $("#bass-button").css({top: '4em', height: '100%'}, 500);
         
             saxClicked = false;
         }
@@ -161,17 +155,15 @@ $(".instrument").click(function(){
         trumpetAudio.get(0).play();
         
         if(!trumpetClicked){
-            $("#trumpet-button").animate({top: '-5em', left: '5em', height: '100%'}, 500);
-            $("#drum-button").animate({top: '28em', left: '13em', height: '10em'}, 500);
-            $("#sax-button").animate({top: '28em', left: '22em', height: '10em'}, 500);
-            $("#bass-button").animate({top: '28em', left: '30em', height: '10em'}, 500);      
+            $("#drum-button").css({top: '17em', height: '10em'}, 500);
+            $("#sax-button").css({top: '17em', height: '10em'}, 500);
+            $("#bass-button").css({top: '17em', height: '10em'}, 500);      
             
             trumpetClicked = true;
         } else{
-            $("#sax-button").animate({top: '11em', left: '3em', height: '100%'}, 500);
-            $("#drum-button").animate({top: '-4em', left: '20em', height: '100%'}, 500);
-            $("#trumpet-button").animate({top: '10em', left: '20em', height: '100%'}, 500);
-            $("#bass-button").animate({top: '-4em', left: '40em', height: '100%'}, 500);
+            $("#sax-button").css({top: '4em', height: '100%'}, 500);
+            $("#drum-button").css({top: '4em', height: '100%'}, 500);
+            $("#bass-button").css({top: '4em', height: '100%'}, 500);
         
             saxClicked = false;
         }
@@ -179,7 +171,6 @@ $(".instrument").click(function(){
     });
 });
 
-*/
 
 function drumCookie(){
     Cookies.set('drum', 'value', { expires: 7 });
