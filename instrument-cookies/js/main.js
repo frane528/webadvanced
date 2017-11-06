7f8e1bf9e0cbf380468fe98eff03215686cb2741
Emily Frank <frane528@newschool.edu>
2017-11-06 19:42:46 +0000

    files

--- instrument-cookies/js/main.js
+++ instrument-cookies/js/main.js
@@ -1,189 +1,115 @@
 $(document).ready(function(){
     
-/*
-$(".instrument").click(function(){
-    
-    $(".instrument").each(function(){
-        
-        $(this).removeClass("big-instrument") //this removes the top left position for each
-        
+  $('.instrument').on('mouseenter', (event) => {
+    $(event.currentTarget).addClass('instrument-active')
+  }).on('mouseleave', event => {
+    $(event.currentTarget).removeClass('instrument-active')
+  })
+  
+  $('.instrument').on('click', event => {
+  $(event.currentTarget).siblings().toggleClass('little-instrument');
     })
     
-    $(this).addClass("big-instrument") //this adds the top left position to the instrument that was clicked on
-    
-    $(".instrument").each(function(){
+  var drumAudio = $("#drum-clip");
+  $("#drum-button").click(function(){drumAudio.get(0).play();
+  });
         
-        if($(this).hasClass("big-instrument")){
-           
-           $(this).css({top: '8em', left: '5em', height: '100%'}) 
+  var saxAudio = $("#sax-clip");
+  $("#sax-button").click(function(){saxAudio.get(0).play();
+  });
+        
+  var bassAudio = $("#bass-clip"); 
+  $("#bass-button").click(function(){ bassAudio.get(0).play();
+  });
+  
+  var trumpetAudio = $("#trumpet-clip"); 
+  $("#trumpet-button").click(function(){ trumpetAudio.get(0).play();
+  });
+});
+
+        
+
+    
+    
+/*
+    Cookies:
+    
+    part 1
+    
+    -check if cookie exists
+        - if cookie exists
+            get information from cookie
+            apply it to instruments
+        
+        - if cookie doesnt exist
+            just continue as normal
+*/
+    
+    //part 2
+    
+    var clicked = []
+    
+    - //setting the cookie
+    - //essentially, on each instrument click you'll need to reset the cookie
+    
+    var clicked = [];
+    
+    /*if(Cookies.getJSON('instruments')){
+        //if this cookie exists
+        
+       clicked = Cookies.getJSON('instruments'); // set the empty array clicked to equal the saved cookie
+        
+        
+        var cookieExists = function(){ //then run a function 
             
-        }else{
-            
-            if($(this).attr("id") == "bass-button"){
-                //specific small position
-                $("#drum-button").css({top: '28em', left: '10em', height: '10em'}, 500);
-                $("#sax-button").css({top: '28em', height: '10em'}, 500);
-                $("#trumpet-button").css({top: '28em', height: '10em'}, 500);
-                
-            }
-            
-            if($(this).attr("id") == "drum-button"){
-                //specific small position
-                $("#bass-button").css({top: '28em', left: '10em', height: '10em'}, 500);
-                $("#sax-button").css({top: '28em', height: '10em'}, 500);
-                $("#trumpet-button").css({top: '28em', height: '10em'}, 500);
-                
-            }
-            
-            if($(this).attr("id") == "sax-button"){
-                //specific small position
-                $("#bass-button").css({top: '28em', left: '10em', height: '10em'}, 500);
-                $("#drum-button").css({top: '28em', height: '10em'}, 500);
-                $("#trumpet-button").css({top: '28em', height: '10em'}, 500);
-                
-            }
-            
-            if($(this).attr("id") == "trumpet-button"){
-                //specific small position
-                $("#bass-button").css({top: '28em', left: '10em', height: '10em'}, 500);
-                $("#drum-button").css({top: '28em', height: '10em'}, 500);
-                $("#sax-button").css({top: '28em', height: '10em'}, 500);
-                
-            }
+            // have a for loop iterate through the clicked array
+            //for each id that's in the array, set it as if it was already clicked
             
         }
         
+        cookiesExists();
         
-    })
-        var drumAudio = $("#drum-clip");
-        $("#drum-button").click(function(){
+        if(clicked.length == 4){
+            
+            //play song
+        }
         
-        drumAudio.get(0).play();
-        });
-        
-        var saxAudio = $("#sax-clip");
-        $("#sax-button").click(function(){
-        
-        saxAudio.get(0).play();
-        });
-        
-        var bassAudio = $("#bass-clip");
-        $("#bass-button").click(function(){
-        
-        bassAudio.get(0).play();
-        });
-})
-*/
-    
-
-    var drumAudio = $("#drum-clip");
-    var drumClicked = false;
+    }
+ 
     
     $("#drum-button").click(function(){
         
-        drumAudio.get(0).play();
+        //preparing for cookie
         
+        var inArray = false;
         
-        if(!drumClicked){
-            $("#bass-button").css({top: '17em', height: '10em'}, 500);
-            $("#sax-button").css({top: '17em', height: '10em'}, 500);
-            $("#trumpet-button").css({top: '18em', height: '10em'}, 500);
+        for (var i = clicked.length; i--; ) {
             
-            drumClicked = true;
-        } else{
-            $("#bass-button").css({top: '4em', height: '100%'}, 500);
-            $("#sax-button").css({top: '4em', height: '100%'}, 500);
-            $("#trumpet-button").css({top: '4em', height: '100%'}, 500);
-        
-            drumClicked = false;
+            if(clicked[i] = "#drum-cookie"){
+                
+                inArray = true;
+            }
         }
-      
-    });
-    
-        var bassAudio = $("#bass-clip");
-        var bassClicked = false;
-    
-        $("#bass-button").click(function(){
-            
-        bassAudio.get(0).play();
         
-        if(!bassClicked){
-            $("#drum-button").css({top: '17em', height: '10em'}, 500);
-            $("#sax-button").css({top: '17em', height: '10em'}, 500);
-            $("#trumpet-button").css({top: '17em', height: '10em'}, 500);
+        if(inArray){ //checking if #drum-button is part of the clicked array
+                
+                inArary = false;
+                //we want to remove #drum-button from array
+                
+                var itemtoRemove = "#drum-button";
+                clicked.splice($.inArray(itemtoRemove, clicked),1);
+                
+                //Cookies.set('instruments', clicked); //reseting the cookie
+
+                
+                console.log("in array", clicked)
             
-            bassClicked = true;
-        } else{
-            $("#drum-button").css({top: '4em', height: '100%'}, 500);
-            $("#sax-button").css({top: '4em', height: '100%'}, 500);
-            $("#trumpet-button").css({top: '4em', height: '100%'}, 500);
+            }else{ //if #drum-button is not part of the array
+                
+                clicked.push("#drum-button")
+                
+                //Cookies.set('instruments', clicked); /resetting the cookie
+                console.log("added to array", clicked)
+                
+            }
         
-            bassClicked = false;
-        }
-      
-    });
-    
-
-        var saxAudio = $("#sax-clip");
-        var saxClicked = false;
-    
-        $("#sax-button").click(function(){
-            
-        saxAudio.get(0).play();
-        
-        if(!saxClicked){
-            $("#drum-button").css({top: '17em', height: '10em'}, 500);
-            $("#trumpet-button").css({top: '18em', height: '10em'}, 500);
-            $("#bass-button").css({top: '17em', height: '10em'}, 500);      
-            
-            saxClicked = true;
-        } else{
-            $("#drum-button").css({top: '4em', height: '100%'}, 500);
-            $("#trumpet-button").css({top: '4em', height: '100%'}, 500);
-            $("#bass-button").css({top: '4em', height: '100%'}, 500);
-        
-            saxClicked = false;
-        }
-      
-    });
-    
-        var trumpetAudio = $("#trumpet-clip");
-        var trumpetClicked = false;
-    
-        $("#trumpet-button").click(function(){
-            
-        trumpetAudio.get(0).play();
-        
-        if(!trumpetClicked){
-            $("#drum-button").css({top: '17em', height: '10em'}, 500);
-            $("#sax-button").css({top: '17em', height: '10em'}, 500);
-            $("#bass-button").css({top: '17em', height: '10em'}, 500);      
-            
-            trumpetClicked = true;
-        } else{
-            $("#sax-button").css({top: '4em', height: '100%'}, 500);
-            $("#drum-button").css({top: '4em', height: '100%'}, 500);
-            $("#bass-button").css({top: '4em', height: '100%'}, 500);
-        
-            saxClicked = false;
-        }
-      
-    });
-});
-
-
-function drumCookie(){
-    Cookies.set('drum', 'value', { expires: 7 });
-}
-
-function bassCookie(){
-    Cookies.set('bass', 'value', { expires: 7 });
-}
-
-function saxCookie(){
-    Cookies.set('sax', 'value', { expires: 7 });
-}
-
-function trumpetCookie(){
-    Cookies.set('trumpet', 'value', { expires: 7 });
-}
