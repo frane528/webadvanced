da65cae8aefbaa6bb9de3665662a8001573a1d3e
Emily Frank <frane528@newschool.edu>
2017-11-09 01:27:12 +0000

    changes

--- instrument-cookies/js/main.js
+++ instrument-cookies/js/main.js
@@ -1,15 +1,155 @@
 $(document).ready(function(){
-    
-  $('.instrument').on('mouseenter', (event) => {
-    $(event.currentTarget).addClass('instrument-active')
-  }).on('mouseleave', event => {
-    $(event.currentTarget).removeClass('instrument-active')
-  })
+   
+   
+   /*
+   COokie has two parts
+   
+   checking if cookie exists when page loads
+   
+   adding to cookie throughout interactions
+   
+   update instrumentArray (so that means either pushing to the array or splicing (i.e. removing) from array)
+        
+        var idOfInstrument = 'idOfInstrument';
+        
+        instrumentArray.push(idOfInstrument)
+        
+        instrumentArray.splice($.inArray(idOfInstrument, instrumentArray),1);
+   
+   update instrumentCookie to equal new instrumentArray
+        Cookies.set('instrumentCookie', instrumentArray)
+        
+   
+   
+   */
+   
+   var instrumentArray = []; //starts empty
+   
+   var playWholeSong = function(){
+       
+       //play song
+   }
+   
+   
+   if(Cookies.getJSON('instrumentCookie')){
+        //if this cookie exists
+        
+       instrumentArray = Cookies.getJSON('instrumentCookie'); // set the empty array clicked to equal the saved cookie
+        console.log("exists!",Cookies.getJSON('instrumentCookie') )
+        if(instrumentArray.length == 4){
+            
+            playWholeSong()
+        
+        }
+   }
+   
+   console.log(instrumentArray)
+   
+   
+   
   
-  $('.instrument').on('click', event => {
-  $(event.currentTarget).siblings().toggleClass('little-instrument');
-    })
+  // click on instrument
+  $('.instrument').click(function() {
+      
+      var idOfInstrument = $(this).attr("id");
+      console.log(idOfInstrument)
+      
+      if ($(this).hasClass('active')){
+          
+          $(this).removeClass('active')
+          $(this).siblings('.instrument').removeClass('inactive')
+          
+          //cookie logic:
+          //want to remove it form the array
+          
+          console.log($.inArray(idOfInstrument, instrumentArray))
+          
+          instrumentArray = instrumentArray.splice($.inArray(idOfInstrument, instrumentArray),1);
+          //alternative is a for loop or an each loop to remove the idOfInstrumetn from array
+          
+          
+          //update cookie
+          Cookies.set('instrumentCookie', instrumentArray, { expires: 7 })
+          
+      }else if($(this).hasClass('inactive')){
+          
+          $(this).addClass('active').removeClass('inactive')
+           $(this).siblings('.instrument').removeClass('active')
+          $(this).siblings('.instrument').addClass('inactive')
+          
+          //cookie logic
+          if($.inArray(idOfInstrument, instrumentArray) !== -1){
+              
+              console.log($.inArray(idOfInstrument, instrumentArray))
+              
+              //if its already in array we do nothing
+              
+          }else if($.inArray(idOfInstrument, instrumentArray) == -1){
+              //if its not in array we want to add it to the array
+              instrumentArray.push(idOfInstrument);
+              // add updated array to the cookie
+              Cookies.set('instrumentCookie', instrumentArray, { expires: 7 });
+              
+          }
+          
+      }else{ //neutral setting
+          
+          $(this).addClass('active')
+          $(this).siblings('.instrument').addClass('inactive')
+          
+          //cookie logic
+          if($.inArray(idOfInstrument, instrumentArray) !== -1){
+              
+              console.log($.inArray(idOfInstrument, instrumentArray))
+              
+              //if its already in array we do nothing
+              
+          }else if($.inArray(idOfInstrument, instrumentArray) == -1){
+              //if its not in array we want to add it to the array
+              instrumentArray.push(idOfInstrument);
+              // add updated array to the cookie
+              Cookies.set('instrumentCookie', instrumentArray, { expires: 7 });
+              
+          }
+      }
     
+    console.log(instrumentArray)
+      
+  })
+      /*
+      
+      click on instrument
+      
+        all instruments can either be
+        1. active
+        2. inactive
+        3. neutral (don't have an active or inactive class)
+      
+      - makes this instrument active
+      - unless this instrument is already active
+      
+      -makes all other instruments small
+      - unless this instrument is already active
+      
+      
+      */
+      
+      // gives the other instruments their opposite class (potential bug)
+  //$(event.currentTarget).siblings().toggleClass('little-instrument');
+    
+    /*
+     if $(this=='.little-instrument'){
+    	$(event.currentTarget).addClass('active')
+    	$(event.currentTarget).siblings().removeClass('inactive');
+    })
+    */
+  
+  
+
+    
+    
+    
+    //audio stuff
   var drumAudio = $("#drum-clip");
   $("#drum-button").click(function(){drumAudio.get(0).play();
   });
@@ -25,6 +165,9 @@ $(document).ready(function(){
   var trumpetAudio = $("#trumpet-clip"); 
   $("#trumpet-button").click(function(){ trumpetAudio.get(0).play();
   });
+  
+  console.log(document.cookie);
+  
 });
 
         
