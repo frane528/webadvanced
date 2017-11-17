a2411dd20ba35809735a938bbea7065cb64c974f
Emily Frank <frane528@newschool.edu>
2017-11-17 17:47:55 +0000

    push

--- responsive-rss/js/main.js
+++ responsive-rss/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
+  
+  /*
 	console.log("RSS - Responsive");
 
     var statement = "select * from feed where url='http://www.theonion.com/rss'"; // here you link the url you need
@@ -7,14 +8,44 @@ $(document).ready(function(){
 
       $("#onion").append("<h3>The Onion RSS Feed</h3>") //append title of rss feed
 
-        for (var i = 0; i < data.query.results.item.length; i++) { // for each result:
+        for (var i = 0; i < data.query.results.item.length; i) { // for each result:
 
         console.log(data.query.results.item[i]) // console log the result's object
 
-        $("#onion").append( "<h2>" + data.query.results.item[i].title + "</h2><p>" + data.query.results.item[i].description + "</p>")
+        $("#onion").append( "<h3>" + data.query.results.item[i].title + "</h3><p>" + data.query.results.item[i].description + "</p>")
 
         }
 
     });
+    */
+    
+    //NYT
+    var url = "https://api.nytimes.com/svc/news/v3/content/content.json";
+    url += '?' + $.param({
+      'api-key': "aaa01caec4f34077939eb30a2af49d87",
+      'url': "https://frane528.github.io/webadvanced/fake-news/index.html"
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
+    
+    //breitbart
+    
+    var url = 'https://newsapi.org/v2/top-headlines?' +
+          'sources=bbc-news&' +
+          'apiKey=6c2b8f7b3357496ab6710a4dd88a77ce';
 
+    var req = new Request(url);
+    
+    fetch(req)
+        .then(function(response) {
+            console.log(response.json());
+        })
 })
+
+
