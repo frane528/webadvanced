bd00bb28a8ddcde900938ed7f46407a1d1e1856e
Emily Frank <frane528@newschool.edu>
2017-12-13 20:01:08 +0000

    changes

--- fake-news/js/main.js
+++ fake-news/js/main.js
@@ -5,6 +5,30 @@ $(document).ready(function(){
     var onionFeed = [],
         timesFeed = [],
         breitbartFeed = [];
+            
+            
+        // var object = {
+            
+        //     "key" : "value",
+            
+        //     "class": "web advanced",
+        //     "school": "Parsons",
+            
+        //     "students" : [{
+                    
+        //             "name": "Emily",
+        //             "hair": "blonde"
+                
+        //     }, "Jack", "Connie"]
+            
+            
+        // }
+        
+        // object.students[0].name
+        
+        
+        
+        
 
     var statement = "select * from feed where url='http://www.theonion.com/rss'"; // here you link the url you need
     $.queryYQL(statement, "json", undefined, function (data) { //function taken from the query-yql library 
@@ -21,18 +45,34 @@ $(document).ready(function(){
         
         }
     })
-    
+  
     
     //breitbart query
      var breitbartUrl = 'https://newsapi.org/v2/top-headlines?sources=breitbart-news&apiKey=6c2b8f7b3357496ab6710a4dd88a77ce';
      
      $.getJSON( breitbartUrl, function(json){
          
+        for (var i = 0; i < 1; i++) {
+        
         console.log("breitbart", json.articles)
+        
+        $("#breitbart").append( "<h3>" + json.articles + "</h3>");
         
         //push breitbart articles to breitbartFeed array
         
-        breitbartFeed.push(json.articles)
+        //breitbartFeed.push(json.articles)
+
+         }
+         
+         console.log("here we are: ", json.articles.length)
+         
+         var articleAmount = json.articles.length; //gets article amount
+         
+         var randomNumber = Math.round(Math.random()*articleAmount) //produces a random number that is less than the total article amount
+         
+         console.log(json.articles[randomNumber]) 
+         
+         breitbartFeed.push(json.articles[randomNumber]) // adds the random articles to the feed
          
      })
      
@@ -50,8 +90,9 @@ $(document).ready(function(){
           method: 'GET',
         }).done(function(result) {
           
+          $("#nyt").append("<h3>" + result + "</h3>");
           
-          console.log(result);
+          console.log("result here: ", result.results);
           
           //push article info to timesFeed array
           
@@ -74,6 +115,8 @@ $(document).ready(function(){
             if (onionFeed.length > 0 && timesFeed.length > 0 && breitbartFeed.length > 0 ){
                 
                 console.log("all three are loaded!")
+                
+                console.log(breitbartFeed[0].description)
                 
                 // have your logic that gets random content from each and appends it into the questionaire
                 
@@ -139,6 +182,9 @@ $(document).ready(function(){
         
         loadedContent()
     
-    
-    
+    //$("#congrats").append( "<h4>CONGRATS</h4><p>This year has been a rollercoaster, but you're keeping your wits about you. Give yourself a pat on the back.</p><a>TRY AGAIN ></a>" );
+    //$("#yikes").append("<h4>YIKES</h4><p>You don't know which way is up and which way is down. The good news is, most people don't either.</p><a>TRY AGAIN ></a>");
+    //$("#hmm").append("<h4>Hmm</h4><p>Your bullshit detector is losing it's strength. Not blaming you, though. Just saying.</p><a>TRY AGAIN ></a>")
 });
+
+ 
