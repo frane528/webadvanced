377defd87f671ef15483ebd19fcea71ea39ce464
Emily Frank <frane528@newschool.edu>
2017-12-15 13:48:37 +0000

    stuff

--- responsive-rss/js/main.js
+++ responsive-rss/js/main.js
@@ -1,1 +1,22 @@
+$(document).ready(function(){
+
+	console.log("RSS - Responsive");
+
+	var statement = "select * from feed where url='http://www.theonion.com/rss'";
+	$.queryYQL(statement, "json", undefined, function (data) {
+	  
+	    for (var i = 0; i < 10; i++) { // for each result:
+
+        console.log(data.query.results.item[i]) // console log the result's object
+        
+         $("#onion").append( "<h3>" + data.query.results.item[i].title + "</h3><p>" + data.query.results.item[i].description + "</p>")
+
+        var articleInfo = data.query.results.item[i];
+
+        onionFeed.push(articleInfo)
+        
+        }
+    })
+
+	});
 
